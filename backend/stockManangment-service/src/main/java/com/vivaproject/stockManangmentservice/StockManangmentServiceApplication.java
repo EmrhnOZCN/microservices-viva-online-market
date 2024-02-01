b89d00875e699b2f28ea21f5ce06299bcc48b691
EmrhnOZCN <emrhnozcnceng@gmail.com>
2024-02-01 14:57:12 +0300

    stockManangmentService-oluşturuldu

---
+++ backend/stockManangment-service/src/main/java/com/vivaproject/stockManangmentservice/StockManangmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vivaproject.stockManangmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManangmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManangmentServiceApplication.class, args);
+	}
+
+}
---
+++ backend/stockManangment-service/src/test/java/com/vivaproject/stockManangmentservice/StockManangmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivaproject.stockManangmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManangmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
