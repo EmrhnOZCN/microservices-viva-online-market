311c355c9eb66199d1aa10495b939b80dc342926
EmrhnOZCN <emrhnozcnceng@gmail.com>
2024-02-01 14:54:46 +0300

    basketservice-oluşturuldu

---
+++ backend/basket-service/src/main/java/com/vivaproject/basketservice/BasketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vivaproject.basketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketServiceApplication.class, args);
+	}
+
+}
