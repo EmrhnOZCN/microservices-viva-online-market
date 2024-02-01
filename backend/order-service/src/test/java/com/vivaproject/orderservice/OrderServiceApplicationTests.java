8c6f88808614119d5543fb4c3114337c4e2068f7
EmrhnOZCN <emrhnozcnceng@gmail.com>
2024-02-01 14:51:40 +0300

    orderservice-oluşturuldu

---
+++ backend/order-service/src/test/java/com/vivaproject/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivaproject.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
