1ec2fe5957dc4768d1948f66331df55bb1c6bec2
EmrhnOZCN <emrhnozcnceng@gmail.com>
2024-02-03 14:20:00 +0300

    tasarım

--- frontend/vivaproject/src/components/Home/Home.js
+++ frontend/vivaproject/src/components/Home/Home.js
@@ -6,11 +6,15 @@ import Category from '../Category/Category';
 import Products from '../Products/Products';
 import Footer from '../Footer/Footer';
 import Chat from '../Chats/Chat';
+import TopicProduct from '../TopicProducts/TopicProduct';
+import Navbar2 from '../Navbar2/Navbar2';
 
 const Home = () => {
   return (
     <div>
+      <Navbar2></Navbar2>
       <Slider></Slider>
+      <TopicProduct></TopicProduct>
       <Category></Category>
       <Products></Products>
       <Footer></Footer>
--- frontend/vivaproject/src/components/Slider/Slider.js
+++ frontend/vivaproject/src/components/Slider/Slider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import slide1 from '../../assets/slide1.jpg';
 import slide2 from '../../assets/slide2.jpg';
@@ -7,13 +7,64 @@ import slidetop from '../../assets/ustslide.jpg';
 import './Slider.css';
 
 const Slider = () => {
+  const sliderRef = useRef(null);
+
+  useEffect(() => {
+    const slider = sliderRef.current;
+    const slides = document.querySelectorAll('.carousel-item');
+    const slideWidth = slides[0].offsetWidth;
+
+    const autoScroll = () => {
+      slider.scrollLeft += slideWidth;
+
+      // Slider'ın sonuna geldiyse, başa dön
+      if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth) {
+        // İlk slayta dön
+        slider.scrollLeft = 0;
+      }
+    };
+
+    const intervalId = setInterval(autoScroll, 3000);
+
+    // Component unmounted olduğunda clearInterval
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const handleNext = () => {
+    const slider = sliderRef.current;
+    const slideWidth = slider.querySelector('.carousel-item').offsetWidth;
+
+    console.log('Next Button Clicked');
+    slider.scrollLeft += slideWidth;
+
+    // Slider'ın sonuna geldiyse, başa dön
+    if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth) {
+      // İlk slayta dön
+      slider.scrollLeft = 0;
+    }
+  };
+
+  const handlePrev = () => {
+    const slider = sliderRef.current;
+    const slideWidth = slider.querySelector('.carousel-item').offsetWidth;
+
+    console.log('Prev Button Clicked');
+    slider.scrollLeft -= slideWidth;
+
+    // Slider'ın başına geldiyse, sona dön
+    if (slider.scrollLeft <= 0) {
+      // Son slayta dön
+      slider.scrollLeft = slider.scrollWidth - slider.clientWidth;
+    }
+  };
+
   return (
     <div className="position-relative">
       {/* Üstteki Resim */}
       <img src={slidetop} alt="Additional Image" className="w-top" style={{ position: 'flex', top: '-10px', zIndex: 1 }} />
 
       {/* Slider */}
-      <div id="carouselExample" className="carousel slide mt-3" data-bs-ride="carousel">
+      <div id="carouselExample" className="carousel slide mt-3" data-bs-ride="carousel" ref={sliderRef}>
         <div className="carousel-inner">
           <div className="carousel-item active">
             <img src={slide1} className="d-block w-100" alt="First slide" />
@@ -25,11 +76,11 @@ const Slider = () => {
             <img src={slide3} className="d-block w-100" alt="Third slide" />
           </div>
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev" onClick={handlePrev}>
           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Previous</span>
         </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next" onClick={handleNext}>
           <span className="carousel-control-next-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Next</span>
         </button>
