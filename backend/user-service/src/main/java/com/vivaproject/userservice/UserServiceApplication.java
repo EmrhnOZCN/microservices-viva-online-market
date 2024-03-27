75a5691ade15943ce804d3acf2b1eededb7b75c7
EmrhnOZCN <emrhnozcnceng@gmail.com>
2024-03-27 19:05:22 +0300

    user service eureka servera kayıt edildi

--- backend/user-service/src/main/java/com/vivaproject/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/vivaproject/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.vivaproject.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
